bdca5bc4837cd117cdd2f2909a9732ac36c2c156
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2026-04-08 08:15:33 -0300

    study: Scanner challenge reading salary values as String and handling decimal separators with replace()

---
+++ estudo_01/src/DesefioComveção.java
@@ -0,0 +1,28 @@
+/*
+O desafio consiste em utilizar o Scanner do Java para capturar 3 
+valores informados como String, correspondentes aos 3 últimos salários. 
+Em seguida, será calculada a média desses valores. O programa deve permitir 
+a entrada de números decimais utilizando vírgula ou ponto como separador 
+decimal.
+*/
+import java.util.Scanner;
+
+public class DesefioComveção {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite os 3 últimos salários separados por espaço:");
+        String valor1 = scanner.next().replace(",", ".");
+        String valor2 = scanner.next().replace(",", ".");
+        String valor3 = scanner.next().replace(",", ".");
+
+        double salario1 = Double.parseDouble(valor1);
+        double salario2 = Double.parseDouble(valor2);
+        double salario3 = Double.parseDouble(valor3);
+
+        double media = (salario1 + salario2 + salario3) / 3;
+
+        System.out.printf("A média dos salários é: %.2f%n", media);
+
+        scanner.close();
+    }
+}
