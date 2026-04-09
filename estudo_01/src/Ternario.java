f39f2e0e159973aeca115f2bc6e24bd179355da8
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2026-04-09 10:39:53 -0300

    study: example of ternary operator for conditional evaluation in Java

---
+++ estudo_01/src/Ternario.java
@@ -0,0 +1,7 @@
+public class Ternario {
+    public static void main(String[] args) {
+        double nota = 7.5;
+        String resultado = nota >= 7.0 ? "Aprovado" : "Reprovado";
+        System.out.println("Resultado: " + resultado);
+    }
+}
